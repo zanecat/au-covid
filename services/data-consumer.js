59155e730ac1a8ee9f508f0ccd45f9b0b184b8cb
Zane's Pi <pi@noreply.com>
2021-08-16 12:18:54 +1000

    reset changes

--- services/data-consumer.js
+++ services/data-consumer.js
@@ -12,56 +12,56 @@ const dataConsumer = async (tweet) => {
     const md = generateMdContent(tweetInfo)
     console.log("md: " + md)
 
-    await exec('git pull', (err) => {
-        console.log(err)
-    })
-    console.log('git pulling...')
-
-    const dirPath = 'mds'
-    await exec('mkdir -p ' + dirPath)
-
-    const path = `mds/${tweetInfo.region}.md`
-    await exec(`chmod +x ${dirPath}`)
-
-    await exec(`echo "${md}" > ${path}`, (err) => {
-        console.log(err)
-        console.log('echo done')
-    })
-
-    console.log('git commit changes')
-    await exec('git add .', (err) => {
-        console.log(err)
-
-    })
-
-    await exec(`git commit -m "uploaded ${path}, ${dateString}"`, (err) => {
-        console.log(err)
-    })
-
-    await exec('git push')
-    console.log('pushed')
-
-
-    // exec('git pull', () => {
-    //     console.log("pull")
-    //     const dirPath = 'mds'
-    //     exec('mkdir -p '+dirPath)
-    //     console.log('mkdir')
-    //     const path = `mds/${tweetInfo.region}.md`
-    //     exec(`chmod +x ${dirPath}`)
-    //     exec(`echo "${md}" > ${path}`, (err) => {
-    //         console.log(err)
-    //         console.log('echo')
-    //         exec('git add .', (err) => {
-    //             console.log(err)
-    //             exec(`git commit -m "uploaded ${path}, ${dateString}"`, (err) => {
-    //                 console.log(err)
-    //                 exec('git push')
-    //                 console.log('pushed')
-    //             })
-    //         })
-    //     })
+    // await exec('git pull', (err) => {
+    //     console.log(err)
     // })
+    // console.log('git pulling...')
+
+    // const dirPath = 'mds'
+    // await exec('mkdir -p ' + dirPath)
+
+    // const path = `mds/${tweetInfo.region}.md`
+    // await exec(`chmod +x ${dirPath}`)
+
+    // await exec(`echo "${md}" > ${path}`, (err) => {
+    //     console.log(err)
+    //     console.log('echo done')
+    // })
+
+    // console.log('git commit changes')
+    // await exec('git add .', (err) => {
+    //     console.log(err)
+
+    // })
+
+    // await exec(`git commit -m "uploaded ${path}, ${dateString}"`, (err) => {
+    //     console.log(err)
+    // })
+
+    // await exec('git push')
+    // console.log('pushed')
+
+
+    exec('git pull', () => {
+        console.log("pull")
+        const dirPath = 'mds'
+        exec('mkdir -p '+dirPath)
+        console.log('mkdir')
+        const path = `mds/${tweetInfo.region}.md`
+        exec(`chmod +x ${dirPath}`)
+        exec(`echo "${md}" > ${path}`, (err) => {
+            console.log(err)
+            console.log('echo')
+            exec('git add .', (err) => {
+                console.log(err)
+                exec(`git commit -m "uploaded ${path}, ${dateString}"`, (err) => {
+                    console.log(err)
+                    exec('git push')
+                    console.log('pushed')
+                })
+            })
+        })
+    })
 }
 
 const extractTweetInfo = (tweet) => {
